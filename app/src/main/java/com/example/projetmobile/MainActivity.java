5ed9441ea641d63b6cae6f274c0370e9c8df2b57
ManonLac <manonlac@live.fr>
2023-03-20 14:06:50 +0100

    1ere partie toolbar

--- app/src/main/java/com/example/projetmobile/MainActivity.java
+++ app/src/main/java/com/example/projetmobile/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToolBar myToolBar =(ToolBar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolBar);
     }
 }
