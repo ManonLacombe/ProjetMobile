6f61f9c4a57e88742174edf93c9c61c1512a6e34
ManonLac <manonlac@live.fr>
2023-03-21 16:18:35 +0100

    début visuel + toolbar de cette page

---
+++ app/src/main/java/com/example/projetmobile/Connexion.java
@@ -0,0 +1,43 @@
+package com.example.projetmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Connexion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+        Toolbar myToolBar2 =(Toolbar) findViewById(R.id.my_toolbar2);
+        setSupportActionBar(myToolBar2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        TextView txt;
+        switch(item.getItemId()){
+            case R.id.connexion:
+                //indications
+                return (true);
+            case R.id.inscription:
+                //indications
+                return (true);
+        }
+        return true;
+    }
+}
