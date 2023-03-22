a7ceace2926d0e03d5cad53eb14024dbcce7ac1b
ManonLac <manonlac@live.fr>
2023-03-22 16:01:12 +0100

    debut bd inscription

--- app/src/main/java/com/example/projetmobile/Inscription.java
+++ app/src/main/java/com/example/projetmobile/Inscription.java
@@ -8,12 +8,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Inscription extends AppCompatActivity {
 
     Intent IntentConnexion;
     Intent IntentInscription;
+
+    private EditText User, Mdp;
+    DBHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,9 @@ public class Inscription extends AppCompatActivity {
         setSupportActionBar(myToolBar3);
         IntentConnexion=new Intent(Inscription.this,Connexion.class);
         IntentInscription=new Intent(Inscription.this,Inscription.class);
+        db = new DBHandler(this);
+        User=(EditText) findViewById(R.id.User);
+        Mdp=(EditText) findViewById(R.id.Mdp);
     }
 
     @Override
@@ -44,5 +53,19 @@ public class Inscription extends AppCompatActivity {
                 return (true);
         }
         return true;
+    }
+
+    public void add(View view) {
+        //méthode appelée lors du clic sur le bouton ajouter
+        String user=User.getText().toString();
+        String mdp=Mdp.getText().toString();
+        db.insertSession(user, mdp);
+        // à placer dans le clic  Toast.makeText(Inscription.this, "Inscription Validé", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
     }
 }
