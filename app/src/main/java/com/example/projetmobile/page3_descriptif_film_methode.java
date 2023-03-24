4a04e6487178bc25811cd9e4a880a6c214ad7a34
fannybezancon <fanny1.bezancon@free.fr>
2023-03-24 10:42:36 +0100

    merge page 3

---
+++ app/src/main/java/com/example/projetmobile/page3_descriptif_film_methode.java
@@ -0,0 +1,14 @@
+package com.example.projetmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page3_descriptif_film_methode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page3_descriptif_film);
+    }
+}
