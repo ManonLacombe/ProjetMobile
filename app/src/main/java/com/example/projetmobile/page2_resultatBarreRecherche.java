5f8c38da4353c2fe34e99f53111fe730d5ba3c8e
fannybezancon <fanny1.bezancon@free.fr>
2023-03-24 10:15:01 +0100

    layout page 3 description film + debut layout page3 affichage liste film recherche

---
+++ app/src/main/java/com/example/projetmobile/page2_resultatBarreRecherche.java
@@ -0,0 +1,16 @@
+package com.example.projetmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page2_resultatBarreRecherche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2_resultat_barre_recherche);
+    }
+
+
+}
